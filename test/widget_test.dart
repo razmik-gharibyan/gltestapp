45d02253cad95288562d038c8a9582fb7dc56dd2
razmik-gharibyan <razmik.gharibyan.olymp@gmail.com>
2023-07-10 10:07:43 +0400

    Edit test message

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,7 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:gltestapp/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Counter increments smoke test ok hold one i pull up', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
